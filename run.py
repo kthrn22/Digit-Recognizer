312daaf1582f2bb77c71f2a49c244cb970f2faf4
NhanTieu <63922382+kthrn22@users.noreply.github.com>
2021-12-02 14:22:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,14 +3,13 @@ from modeling import *
 
 class args:
     fold = 0
-    test_split = 0.1
     hidden_size = 256
     dropout_probs = 0.4
     num_splits = 5
-    batch_size = 64
     patience = 5
     epochs = 10
-    batch_size = 64
+    train_batch_size = 64
+    val_batch_size = 64
 
 df = pd.read_csv('./train.csv')
 df = create_kfold(df, num_splits = args.num_splits)
@@ -23,4 +22,4 @@ X_val, y_val = preprocess(df_valid, mode = 'train')
 
 model = Model(args.dropout_probs, args.hidden_size)
 model.train((X_train, y_train), (X_val, y_val), patience = args.patience, epochs = args.epochs, 
-                                batch_size = args.batch_size)
+                    train_batch_size = args.train_batch_size, val_batch_size = args.val_batch_size)
